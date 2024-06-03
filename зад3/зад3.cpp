9200be55a0d5df10d10248d2686ec757b8406068
Delyan Pavlov <delyan.pavlov@outlook.com>
2024-06-03 08:17:13 +0300

    Update зад3.cpp

--- зад3/зад3.cpp
+++ зад3/зад3.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int n, b, num = 0;
+	cin >> n >> b;
+	int a;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		if (a == b)
+		{
+			num++;
+		}
+	}
+	cout << num;
+}
